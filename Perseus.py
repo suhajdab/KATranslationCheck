b7705042091216282907c9d1a5fc8d7f7339dfb6
Uli Köhler <ulikoehler@online.de>
2016-01-02 02:15:15 +0100

    Added code to get valid Perseus commands

---
+++ Perseus.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import requests
+import re
+
+def getKAPerseusCommands():
+    """Get a list of valid commands for KA Perseus"""
+    txt = requests.get("https://github.com/Khan/perseus/raw/master/lib/katex/katex.js").text
+    rgx = re.compile(r'"\\+([A-Za-z0-9]+)"')
+    nonRGX = re.compile(r'u[0-9a-f]{4}')
+    return list(filter(lambda s: s and not nonRGX.match(s), re.findall(rgx, txt)))
