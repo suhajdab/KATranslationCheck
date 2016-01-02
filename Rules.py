ece1440c52dff67f023cb0a584b84121dbb26467
Uli Köhler <ulikoehler@online.de>
2016-01-02 15:35:45 +0100

    Added compat mode regex log

--- Rules.py
+++ Rules.py
@@ -67,7 +67,7 @@ def importRulesForLanguage(lang, basedir="."):
     moduleName = "rules.{0}".format(lang)
     print(black("Reading rules from {0}".format(moduleName), bold=True))
     langModule = importlib.import_module(moduleName)
-    print(black("Found {0} rules for language {1}".format(len(langModule.rules), lang), bold=True))
+    print(black("Found {0} rules for language {1} ({2} in compatibility mode)".format(len(langModule.rules), lang, reCompiler.numCompatRegex), bold=True))
     return langModule.rules
 
 _extractImgRegex = reCompiler.compile(r"(https?://ka-perseus-graphie\.s3\.amazonaws\.com/[0-9a-f]{40,40}\.(png|svg))")
