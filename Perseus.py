a682588be10c2e0774bf58dfeb22a3dca111bd59
Uli Köhler <ulikoehler@online.de>
2016-01-02 02:15:53 +0100

    More docs

--- Perseus.py
+++ Perseus.py
@@ -6,5 +6,5 @@ def getKAPerseusCommands():
     """Get a list of valid commands for KA Perseus"""
     txt = requests.get("https://github.com/Khan/perseus/raw/master/lib/katex/katex.js").text
     rgx = re.compile(r'"\\+([A-Za-z0-9]+)"')
-    nonRGX = re.compile(r'u[0-9a-f]{4}')
+    nonRGX = re.compile(r'u[0-9a-f]{4}')  # Skip unicode escapes, e.g. u25ca
     return list(filter(lambda s: s and not nonRGX.match(s), re.findall(rgx, txt)))
