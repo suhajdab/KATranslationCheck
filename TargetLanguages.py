12bcaecfc676d1d2a9ac13f33aef78d13bf1ba82
Uli Köhler <ulikoehler@online.de>
2016-01-13 18:36:44 +0100

    Added missing target languages

---
+++ TargetLanguages.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Manually curated list of target languages for Crowdin.
+
+Testable e.g. with this URL:
+https://crowdin.com/translation/phrase?id=2940777&project_id=10880&target_language_id=11
+"""
+
+targetLanguages = {
+    "es": 1, # Also 2, 3?, 8, 36, 43, 54, 57, 58, 60
+    "cn": 4, # Also 55, 56
+    "ar": 5,
+    "bg": 6,
+    "cz": 9,
+    "da": 10,
+    "de": 11,
+    "gr": 12,
+    "fi": 18,
+    "he": 21,
+    "hu": 22,
+    "hu": 22,
+    "ar": 23,
+    "it": 24,
+    "jp": 25,
+    "ka": 26,
+    "kr": 27,
+    "lt": 29,
+    "mn": 31,
+    "nl": 33,
+    "pl": 38,
+    "pt": 40,
+    "ru": 41,
+    "sk": 42,
+    "sr": 45,
+    "sv-SE": 48,
+    "tr": 50,
+    "uk": 52,
+    "ur-PK": 59,
+    # IDs > 60 not checked
+}
