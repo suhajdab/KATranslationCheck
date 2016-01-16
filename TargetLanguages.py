7c74db270e614768b08f775bdcf48b021f29945c
alani <alain.schaefer@gmail.com>
2016-01-16 11:52:24 +0900

    Update TargetLanguages.py

--- TargetLanguages.py
+++ TargetLanguages.py
@@ -7,33 +7,37 @@ https://crowdin.com/translation/phrase?id=2940777&project_id=10880&target_langua
 """
 
 targetLanguages = {
-    "es": 1, # Also 2, 3?, 8, 36, 43, 54, 57, 58, 60
-    "cn": 4, # Also 55, 56
+    "fr": 2,
+    "es-ES": 3, # Also 1, 2, 3?, 8, 36, 43, 54, 57, 58, 60
     "ar": 5,
-    "bg": 6,
-    "cz": 9,
+    "bg": 7,
+    "cs": 9,
     "da": 10,
     "de": 11,
-    "gr": 12,
+    "el": 12,
     "fi": 18,
     "he": 21,
     "hu": 22,
-    "ar": 23,
     "it": 24,
-    "jp": 25,
+    "ja": 25,
     "ka": 26,
-    "kr": 27,
+    "ko": 27,
     "lt": 29,
     "mn": 31,
     "nl": 33,
     "pl": 38,
     "pt": 40,
+    "pt-BR": 63,
     "ru": 41,
     "sk": 42,
     "sr": 45,
     "sv-SE": 48,
     "tr": 50,
     "uk": 52,
+    "zh-TW": 56, 
+    "zh-CN": 55,    
     "ur-PK": 59,
+    "th": 82,
+    "nb": 155,    
     # IDs > 60 not checked
 }
