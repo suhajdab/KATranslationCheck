2f60df30d357422cb83c3176a2b872d56ec1d4f3
Uli Köhler <ulikoehler@online.de>
2016-01-31 02:47:38 +0100

    Fix UTC error in lint fetcher

--- IMAPLint.py
+++ IMAPLint.py
@@ -57,7 +57,8 @@ def fetchEMail(credentials):
             date = date[len("Date: "):]
         if date.endswith("(PST)"):
             date = date[:-len("(PST)")].strip()
-        #print(date)
+        elif date.endswith("(UTC))"):
+            date = date[:-len("(UTC)")].strip()
         date = datetime.strptime(date, "%a, %d %b %Y %H:%M:%S %z")
         #
         match = rgx.match(subject)
