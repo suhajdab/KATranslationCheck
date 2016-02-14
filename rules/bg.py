7f5c2a6491f6984b01c3a4b6666ad27e348d07a0
Uli Köhler <ulikoehler@online.de>
2016-02-14 02:48:26 +0100

    Try to fix bg issues

--- rules/bg.py
+++ rules/bg.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 # coding: utf-8
 from Rules import *
-from ImageAliases import readImageAliases
-
 
 rules, rule_errors = readRulesFromGoogleDocs("1Jmt2Uyo_rTLaiCO5C253nVQom9LuJr3rPYtZYDwe2c0")
-
-rule_errors = []
 
 if __name__ == "__main__":
     print("Counting %d rules" % len(rules))
