bc3d9867d523270e1becd2e86199c0a34ce88275
Uli Köhler <ulikoehler@online.de>
2016-02-14 03:43:40 +0100

    Fix perseus parser

--- Perseus.py
+++ Perseus.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import requests
 import re
+import os
 
 def getKAPerseusCommands():
     """Get a list of valid commands for KA Perseus"""
@@ -8,3 +9,18 @@ def getKAPerseusCommands():
     rgx = re.compile(r'"\\+([A-Za-z0-9]+)"')
     nonRGX = re.compile(r'u[0-9a-f]{4}')  # Skip unicode escapes, e.g. u25ca
     return list(filter(lambda s: s and not nonRGX.match(s), re.findall(rgx, txt)))
+
+def getCachedKAPerseusCommands():
+    cachefile = os.path.join("cache", "perseus-commands.txt")
+    if os.path.isfile(cachefile):
+        with open(cachefile) as cachein:
+            return [s.strip() for s in cachein.read().split("\n")]
+    else:
+        cmds = getKAPerseusCommands()
+        with open(cachefile, "w") as cacheout:
+            cacheout.write("\n".join(cmds))
+        return cmds
+
+
+if __name__ == "__main__":
+    print(getCachedKAPerseusCommands())
