905825a9837a062b40be2f981db58a49b61251ac
Uli Köhler <ulikoehler@online.de>
2016-08-19 21:41:29 +0200

    Try to fix graphie image finder

--- FindImages.py
+++ FindImages.py
@@ -6,8 +6,8 @@ from check import readPOFiles
 import cffi_re2 as re2
 import simplejson as json
 
-imageRegex = re2.compile(r"https?://ka-perseus-images\.s3\.amazonaws.com/([a-z0-9]+)\.(jpeg|jpg|png)")
-graphieRegex = re2.compile(r"web+graphie://ka-perseus-graphie\.s3\.amazonaws.com/([a-z0-9]+)")
+imageRegex = re2.compile(r"https?://ka-perseus-(images|graphie)\.s3\.amazonaws.com/([a-z0-9]+)\.(jpeg|jpg|png)")
+graphieRegex = re2.compile(r"web\+graphie://ka-perseus-graphie\.s3\.amazonaws.com/([a-z0-9]+)")
 
 images = []
 graphie = []
@@ -17,11 +17,11 @@ def findInPO(po):
         engl = entry.msgid
         trans = entry.msgstr
 
-        for hit in imageRegex.findall(engl) + graphieRegex.findall(trans):
-            images.append("{}.{}".format(hit[0], hit[1]))
+        for hit in imageRegex.findall(engl) + imageRegex.findall(trans):
+            images.append("{}.{}".format(hit[1], hit[2]))
 
         for hit in graphieRegex.findall(engl) + graphieRegex.findall(trans):
-            graphie.append(hit[0])
+            graphie.append(hit)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
