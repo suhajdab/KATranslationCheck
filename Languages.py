e41c60a61fba245bdb4d8555d1578e13bbd9f67b
Uli Köhler <ulikoehler@online.de>
2016-10-24 23:23:35 +0200

    Fetch language IDs in parallel

--- Languages.py
+++ Languages.py
@@ -3,19 +3,33 @@ import requests
 import re
 import json
 import os.path
+from ansicolor import black
+from multiprocessing import Pool
+
+def fetchLanguageID(lang):
+    """
+    Fetch the crowdin language ID
+    """
+    response = requests.get("https://crowdin.com/project/khanacademy/{0}/activity".format(lang))
+    return int(re.search(r"GLOBAL_CURRENT_LANGUAGE_ID\s+=\s+(\d+)", response.text).group(1))
 
 def findAllLanguages():
+    """
+    Acquire a dictionary language -> lang ID from Crowdin
+    """
     "Find a list of Crowdin language codes to which KA is translated to"
+    # Acquire language list
+    print(black("Fetching language list...", bold=True))
     response = requests.get("https://crowdin.com/project/khanacademy")
     txt = response.text
-    langs = {}
-    for lang in re.findall(r"https?://[a-z0-9]*\.cloudfront\.net/images/flags/([^\.]+)\.png", txt):
-        if lang == "en-US": continue
-        print("Fetching language ID for {0}".format(lang))
-        response = requests.get("https://crowdin.com/project/khanacademy/{0}/activity".format(lang))
-        langid = int(re.search(r"GLOBAL_CURRENT_LANGUAGE_ID\s+=\s+(\d+)", response.text).group(1))
-        langs[lang] = langid
-    return langs
+    langs = re.findall(r"https?://[a-z0-9]*\.cloudfront\.net/images/flags/([^\.]+)\.png", txt)
+    print(black("Fetching language IDs...", bold=True))
+    # English is the source language
+    if "en-US" in langs:
+        langs.remove("en-US")
+    # Fetch lang IDs in parallel
+    pool = Pool(32)
+    return dict(zip(langs, pool.map(fetchLanguageID, langs)))
 
 langsFile = "cache/languages.json"
 
