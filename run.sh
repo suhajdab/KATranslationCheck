56413f31db4c06deb9d3bc2858594b1eab97ed8b
Uli Köhler <ulikoehler@online.de>
2017-11-15 17:21:47 +0100

    Added run script to run stuff in serial

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Download
+./katc.py -l de update-translations -j 32
+./katc.py -l pt-BR update-translations -j 32
+./katc.py -l bg update-translations -j 32
+./katc.py -l sv-SE update-translations -j 32
+./katc.py -l ja update-translations -j 32
+./katc.py -l ka update-translations -j 32
+
+# Render
+./katc.py -l de render -f 2_hig
+#./katc.py -l pt-BR render -f 2_high
+./katc.py -l bg render -f 2_high
+./katc.py -l sv-SE render -f 2_high
+./katc.py -l ja render -f 2_high
+./katc.py -l ka render -f 2_high
